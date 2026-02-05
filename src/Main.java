0242860001f071323a36ed06d70bc76d196c15f3
joaofuzeto <fuzeto.jf@gmail.com>
2026-02-05 11:48:21 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -16,8 +18,8 @@ public class Main {
         curso2.setDescricao("descrição curso JavaScript");
         curso2.setCargaHoraria(4);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
+        /*System.out.println(curso1);
+        System.out.println(curso2);*/
 
         Mentoria mentoria1 = new Mentoria();
         Mentoria mentoria2 = new Mentoria();
@@ -26,7 +28,33 @@ public class Main {
         mentoria1.setDescricao("Descrição mentoria Java");
         mentoria1.setData(LocalDate.now());
 
-        System.out.println(mentoria1);
+        /*System.out.println(mentoria1);*/
+
+        Bootcamp bootcamp1 = new Bootcamp();
+        bootcamp1.setNome("Bootcamp Java Developer");
+        bootcamp1.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp1.getConteudos().add(curso1);
+        bootcamp1.getConteudos().add(curso2);
+        bootcamp1.getConteudos().add(mentoria1);
+
+        Dev developer1 = new Dev();
+        developer1.setNome("João");
+        developer1.inscreverBootcamp(bootcamp1);
+        System.out.println("Conteúdos inscritos João: " + developer1.getConteudosInscritos());
+        developer1.progredir();
+        System.out.println("---------");
+
+        System.out.println("Conteúdos inscritos João: " + developer1.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos João: " + developer1.getConteudosConcluidos());
+        System.out.println("---------");
+
+        Dev developer2 = new Dev();
+        developer2.setNome("Monique");
+        developer2.inscreverBootcamp(bootcamp1);
+        System.out.println("Conteúdos inscritos Monique: " + developer2.getConteudosInscritos());
+
+        System.out.println("---------");
+        System.out.println("Conteúdos concluídos Monique: " + developer2.getConteudosConcluidos());
 
     }
 }
